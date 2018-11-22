5e4d4003291ac37101f62ea57fb3a5e01aa59072
FelipeMDB <42344147+FelipeMDB@users.noreply.github.com>
2018-11-22 12:18:54 -0200

    Add files via upload

--- 02-Classes/Servidor.java
+++ 02-Classes/Servidor.java
@@ -4,6 +4,16 @@ public class Servidor
 {
 	public static void Main(String[] args)
 	{
+		Salas salas = new Salas();
+		ServerSocket pedido = new ServerSocket("123.45.67.89", 12321) // ip e conexao
 
+		for(;;)
+		{
+			Socket conexao = pedido.accept();
+			CuidadoraDeUsuario cuidadora = new CuidadoraDeUsuario();
+			cuidadora.start();
+
+			//aqui pode ter outras coisas
+		}
 	}
 }
