5b67336284be763c1f22bd01d6ff57757c1ee9d7
FelipeMDB <42344147+FelipeMDB@users.noreply.github.com>
2018-11-28 16:25:21 -0200

    Add files via upload

--- 02-Classes/Janela.java
+++ 02-Classes/Janela.java
@@ -5,16 +5,18 @@ import java.net.*;
 import java.io.*;
 
 
+
 //TUDO QUE ESTÁ COMENTADO É PORQUE PODE MUDAR OU QUE NÃO HÁ CERTEZA DA UTILIZAÇÃO
 public class Janela
 {
 	// O QUE DECLARAR?
 	private JFrame janela = new JFrame("Sala");
-	private JLabel lblMsg = new JLabel("Mensagens:", JLabel.LEFT);
 	private JTextField txtEnvia = new JTextField();
 	private JTextArea AreaDeConversa = new JTextArea();	//Área na qual são escritas as mensagens
 	private JButton btnEnviar = new JButton();	//envia mensagem
 	private JButton btnSair = new JButton();	//sair da sala atual
+	private JButton btnSalasDisp = new JButton();
+	private JButton btnTeste = new JButton();  //ete botao deve ser apagado antes de enviar o projeto
 	private ServerSocket pedido;
 	private Socket conexao;
 	private ObjectOutputStream transmissor;
@@ -25,12 +27,13 @@ public class Janela
 		{
 			public void componentResized(ComponentEvent e)
 			{
-				lblMsg.setFont (new Font("Arial", Font.PLAIN, Math.min(janela.getHeight(),janela.getWidth())*26/220));
 
-				Font fonte = new Font("Arial", Font.PLAIN, Math.min(janela.getHeight(),janela.getWidth())*22/220);
+				Font fonte = new Font("Times New Roman", Font.PLAIN, Math.min(janela.getHeight(),janela.getWidth())*22/950);
 
 				btnEnviar.setFont (fonte);
 				btnSair.setFont(fonte);
+				btnSalasDisp.setFont(fonte);
+				btnTeste.setFont(fonte);
 			}
 
 			public void componentMoved(java.awt.event.ComponentEvent e)
@@ -57,41 +60,56 @@ public class Janela
 	*/
 
 
-	public Janela(Socket s) throws Exception
+	public Janela(/*Socket s*/) throws Exception
 	{
-			conexao = s;
-			transmissor = new ObjectOutputStream(conexao.getOutputStream());
-			receptor = new ObjectInputStream(conexao.getInputStream());
+			//conexao = s;
+			//transmissor = new ObjectOutputStream(conexao.getOutputStream());
+			//receptor = new ObjectInputStream(conexao.getInputStream());
 			design();
 	}
 
 
 	private void design()
 	{
-			this.lblMsg.setFont(new Font("Arial", Font.PLAIN, 26));
 
-			JPanel painel = new JPanel();
-			painel.setLayout (new GridLayout(1,2));
+			JPanel painelSul = new JPanel();
+			painelSul.setLayout (new GridLayout(1,2));
 
-			Font fonte = new Font ("Arial", Font.PLAIN, 20);
+			JPanel painelNorte = new JPanel();
+			painelNorte.setLayout(new GridLayout(1,2));
+
+			JPanel painelSalas = new JPanel();
+			painelSalas.setLayout(new GridLayout(50,1));
+
+			JPanel painelConversas = new JPanel();
+			painelConversas.setLayout(new GridLayout(50,1));
+
 			//TratadorDeEvento tratador = new TratadorDeEvento();
 
-			this.btnEnviar.setFont(fonte);
 			this.btnEnviar.setText("Enviar");
 			//this.btnEnviar.addActionListener(tratador);
 
-			this.btnSair.setFont(fonte);
+
 			this.btnSair.setText("Sair");
 			//this.btnSair.addActionListener(tratador);
 
-			painel.add(txtEnvia);
-			painel.add(btnEnviar);
+			this.btnSalasDisp.setText("Salas");
+			//this.btnSair.addActionListener(tratador);
 
-			this.janela.setSize (220,300);
+
+			painelSul.add(txtEnvia);
+			painelSul.add(btnEnviar);
+
+			painelNorte.add(btnSair);
+			painelNorte.add(btnSalasDisp);
+
+			this.janela.setSize (900,800);
 			this.janela.getContentPane().setLayout(new BorderLayout());
 
-			this.janela.add(this.lblMsg,BorderLayout.NORTH);
-			this.janela.add(painel,BorderLayout.SOUTH);
+			this.janela.add(painelSul,BorderLayout.SOUTH);
+			this.janela.add(painelNorte,BorderLayout.NORTH);
+			this.janela.add(painelSalas,BorderLayout.EAST);
+			this.janela.add(painelConversas,BorderLayout.WEST);
 			this.janela.add(AreaDeConversa, BorderLayout.CENTER);
 
 			this.janela.addComponentListener (new TratadorDeRedimensionamento());
