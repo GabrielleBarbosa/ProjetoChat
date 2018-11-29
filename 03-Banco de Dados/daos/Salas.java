b3d162968e3159b8138bef58f285d88e5e7b0e41
ChrisAlle <42540844+ChrisAlle@users.noreply.github.com>
2018-11-29 11:37:28 -0200

    Update Salas.java

--- 03-Banco de Dados/daos/Salas.java
+++ 03-Banco de Dados/daos/Salas.java
@@ -1,6 +1,61 @@
+package Banco.dao;
 
+import java.sql.*;
+import bd.*;
+import bd.core.*;
+import bd.dbos.*;
 
 public class Salas
 {
-    ArrayList<sala>;
+    public static MeuResultSet getSalas() throws Exception
+    {
+        MeuResultSet resultado = null;
+        
+        try
+        {
+            String sql;
+            
+            sql= "SELECT * FROM SALA";
+            
+            BDSQLServer.COMANDO.prepareStatement (sql);
+            
+            resultado = BDSQLServer.COMANDO.executeQuery();
+        }
+        catch(SQLException erro)
+        {
+            throw new Exception ("Erro ao recuperar salas");
+        }
+        
+        return resultado;
+    }
+    
+    public static MeuResultSet getSala(int codigo) throws Exception
+    {
+        Sala sala = null
+            
+        try
+        {
+            String sql - "SELECT * SALA WHERE CODIGO = ?";
+            
+            BDSQLServer.COMANDO.prepareStatement (sql);
+            
+            BDSQLServer.COMANDO.setInt (1, codigo);
+            
+            MeuResultSet resultado = (MeuResultSet)BDSQLServer.COMANDO.excecuteQuery();
+            
+            if(!resultado.first())
+                throw new Exception ("Sala inexistente");
+            
+            sala = new sala(resultado.getString("NOME"),
+                            resultado.getInt("QTDMAX"),
+                            resultado.getString("TEMA"),
+                            resultado.getINT("CODSALA")); 
+        }
+        catch(SQLException erro)
+        {
+            throw new Exception ("Erro ao procurar sala");
+        }
+        
+        return sala;
+    }
 }
