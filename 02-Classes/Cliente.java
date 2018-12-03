57a0e14e137e52236c64ab9e8cf4aa0dbf4e439a
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2018-12-03 13:54:43 -0200

    Add files via upload

--- 02-Classes/Cliente.java
+++ 02-Classes/Cliente.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.net.*;
+import java.util.ArrayList;
 
 public class Cliente //instancia janela
 {
@@ -13,10 +14,58 @@ public class Cliente //instancia janela
 			//Socket conexao = new Socket("123.45.67.89",12321);   //ip e porta
 			//colocar um receptor pra enviar pra janela
 			Janela janela = new Janela(/*conexao*/);
+			ArrayList<String> salas = new ArrayList(6);
+			salas.add("oi");
+			salas.add("tchau");
+			salas.add("boa tarde");
+			janela.mostrarSalas(salas);
+
+			janela.mostrarAvisoDeSalaCheia();
+
+			/*for(;;)
+			{
+				Enviavel recebido = receptor.readObject();
+
+				if(recebido instanceof SalasDisponiveis)
+				   janela.mostrarSalas(((SalasDisponiveis)recebido).getNomeSala());
+
+					else if(recebido instanceof AvisoDeSalaCheia)
+						janela.mostrarAvisoDeSalaCheia();
+
+						else if(recebido instanceof AvisoDeSalaInvalida)
+							janela.mostrarAvisoDeSalaInvalida();
+
+							else if(recebido instanceof AvisoDeSalaEscolhidaComSucesso)
+								break;
+			}
+
+			janela.mostrarEscolhaDeNome();
 			for(;;)
 			{
-				//escolhaDeSala();
+				Enviavel recebido = receptor.readObject();
+
+				if(recebido instanceof AvisoDeNomeEscolhidoComSucesso)
+					break;
+
+				else if (recebido instanceof AvisoDeNomeExistente)
+					janela.mostrarAvisoDeNomeExistente();
 			}
+
+			janela.mostrarDesingDeChat();
+
+			for(;;)
+			{
+				Enviavel recebido = receptor.readObject();
+
+				if(recebido instanceof Mensagem)
+					janela.mostra(((Mensagem)recebido).getMensagem());
+
+				else if(recebido instanceof AvisoDeEntradaNaSala)
+					janela.mostraEntrada(((AvisoDeEntradaNaSala)recebido).getRemetente());
+
+				else if(recebido instanceof AvisoDeSaidaDaSala)
+					janela.mostraSaida(((AvisoDeSaidaDaSala)recebido).getRemetente());
+			}*/
 	    }
 	    catch(Exception err)
 	    {}
--- 02-Classes/Janela.java
+++ 02-Classes/Janela.java
@@ -3,8 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.net.*;
 import java.io.*;
-
-
+import java.util.ArrayList;
 
 //TUDO QUE ESTÁ COMENTADO É PORQUE PODE MUDAR OU QUE NÃO HÁ CERTEZA DA UTILIZAÇÃO
 public class Janela
@@ -18,7 +17,6 @@ public class Janela
 	private JButton btnSair = new JButton();	//sair da sala atual
 	private JButton btnSalasDisp = new JButton();
 	private JButton btnTeste = new JButton();  //ete botao deve ser apagado antes de enviar o projeto
-	private ServerSocket pedido;
 	private Socket conexao;
 	private ObjectOutputStream transmissor;
 	private ObjectInputStream receptor;
@@ -29,15 +27,17 @@ public class Janela
 		{
 			public void componentResized(ComponentEvent e)
 			{
-				Font fonte = new Font("Times New Roman", Font.PLAIN, Math.min(janela.getHeight(),janela.getWidth())*22/950);
+				Font fonte = new Font("Times New Roman", Font.PLAIN, Math.min(janela.getHeight(),janela.getWidth())*22/600);
 				btnEnviar.setFont(fonte);
 				btnSair.setFont(fonte);
 				btnSalasDisp.setFont(fonte);
 				btnTeste.setFont(fonte);
 				lblNomeUsuario.setFont(fonte);
-				ok.setFont(fonte);
-				Salas.setFont(fonte);
+				btnOK.setFont(fonte);
+				lblSalas.setFont(fonte);
 				escolhaSala.setFont(fonte);
+				lblSeuNome.setFont(fonte);
+				txtEscrevaNome.setFont(fonte);
 			}
 
 			public void componentMoved(java.awt.event.ComponentEvent e)
@@ -73,10 +73,7 @@ public class Janela
 	}
 
 
-	public void escolhaDeSala()
-	{
 
-	}
 
 
 	private void Criacao()
@@ -122,44 +119,105 @@ public class Janela
 		this.janela.add(AreaDeConversa, BorderLayout.CENTER);
 	}
 
-
 	//objetos do primeiro panel que abre
-	private JLabel Salas = new JLabel("Escolha uma Sala:");
+	private JLabel lblSalas = new JLabel("Escolha uma Sala:");
+	private JLabel lblAvisoErros = new JLabel("A sala escolhida se encontra cheia!");
+	private JLabel lblSeuNome = new JLabel("Escolha um Nome:");
 	private JComboBox escolhaSala = new JComboBox();
-	private JButton ok = new JButton();
+	private JButton btnOK = new JButton();
+	private JPanel painelEscolhaDeSala = new JPanel();
+	private JPanel painelEscolhaDeNome = new JPanel();
+	private JTextArea txtEscrevaNome = new JTextArea();
+	/////////////////////////////////////////////////////
 
 	private void design()
 	{
 
-		JPanel painelEscolhaDeSala = new JPanel();
 		painelEscolhaDeSala.setLayout(new FlowLayout());
 
+		btnOK.setText("OK");
+		TratadorDeEvento  tratador = new TratadorDeEvento();
+        btnOK.addActionListener(tratador);
 
-		ok.setText("OK");
-
-
-		painelEscolhaDeSala.add(Salas);
+		painelEscolhaDeSala.add(lblSalas);
 		painelEscolhaDeSala.add(escolhaSala);
-		painelEscolhaDeSala.add(ok);
+		painelEscolhaDeSala.add(btnOK);
 
-		this.janela.setSize(900,800);
+		this.janela.setSize(600,500);
 		this.janela.getContentPane().setLayout(new BorderLayout());
 
-		this.janela.add(painelEscolhaDeSala,BorderLayout.CENTER);
+		this.janela.add(painelEscolhaDeSala,BorderLayout.NORTH);
+		this.janela.add(painelEscolhaDeNome,BorderLayout.CENTER);
 
 		this.janela.addComponentListener (new TratadorDeRedimensionamento());
 		this.janela.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		this.janela.setVisible(true);
 	}
-	/*
-	public void mostra(String textoR)
+
+
+	public void mostrarSalas(ArrayList<String> salas)
 	{
-		AreaDeConversa.append(textoR + "\n");
+		for(int i=0; i<salas.size();i++)
+			escolhaSala.addItem(salas.get(i));
+	}
+
+	public void mostrarAvisoDeSalaCheia()
+	{
+		painelEscolhaDeNome.add(lblAvisoErros);
+	}
+
+	public void mostrarEscolhaDeNome()
+	{
+		escolhaSala.setEnabled(false);
+		btnOK.setText("Confirmar");
+		painelEscolhaDeSala.remove(btnOK);
+		painelEscolhaDeNome.remove(lblAvisoErros);
+		painelEscolhaDeNome.add(lblSeuNome);
+		painelEscolhaDeNome.add(txtEscrevaNome);
+		painelEscolhaDeNome.add(btnOK);
+		txtEscrevaNome.setColumns(10);
+	}
+
+	/*
+	public void mostra(String textoR, String remetente)
+	{
+		AreaDeConversa.append(remetente + ": " + textoR + "\n");
 	}
 	*/
+
+	private class TratadorDeEvento implements ActionListener
+	{
+		public void trateClickNoBotaoOK()
+		{
+			mostrarEscolhaDeNome();
+			String s = escolhaSala.getSelectedItem()+"";
+			//transmissor.writeObject(new EscolhaDeSala(s));
+			//transmissor.flush;
+		}
+
+		public void trateClickNoBotaoConfirmar()
+		{
+			String s = txtEscrevaNome.getText();
+			//transmissor.writeObject(new EscolhaDeSala(s));
+			//transmissor.flush;
+		}
+
+
+		public void actionPerformed (ActionEvent e)
+		{
+			String comando = e.getActionCommand();
+			if(comando == "OK")
+			{
+				trateClickNoBotaoOK();
+			}
+			if(comando == "Confirmar")
+			{
+				trateClickNoBotaoConfirmar();
+			}
+
+		}
+	}
 }
-
-
 
 
 //this.janela.remove       --> elimina o objeto em questão
